ae5b9c90b91ee650228a8057e3a390335c05a95a
Dominique Otchoumou <otchoumou.ang@gmail.com>
2025-09-07 08:04:00 +0000

    test ok

--- ODMapi/obj/Debug/net9.0/ODMapi.AssemblyInfo.cs
+++ ODMapi/obj/Debug/net9.0/ODMapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ODMapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e76d63ca35cea78ce63c576edfd700b3feb3a957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e4371c3e532591e6112b8f5663bb8ad0669e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("ODMapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ODMapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
